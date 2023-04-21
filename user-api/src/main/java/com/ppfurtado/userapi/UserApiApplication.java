de8e540b3ef5c81eb9300beaa23a7038e9fad3ac
Pedro Furtado <pedro.nazare@pdcase.com.br>
2023-04-20 23:26:09 -0300

    feat: iniciando api com users

--- user-api/src/main/java/com/ppfurtado/userapi/UserApiApplication.java
+++ user-api/src/main/java/com/ppfurtado/userapi/UserApiApplication.java
@@ -1,13 +1,32 @@
 package com.ppfurtado.userapi;
 
+import com.ppfurtado.userapi.domain.User;
+import com.ppfurtado.userapi.repositories.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
-public class UserApiApplication {
+public class UserApiApplication implements CommandLineRunner {
+
+	private final UserRepository userRepository;
+
+	public UserApiApplication(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserApiApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		userRepository.saveAll(List.of(
+				new User(null, "Pedro", "furtado.pedropaulo@gmail.com", "123456", "100"),
+				new User(null, "Kelly", "kelly2Vilella@gmail.com", "123456", "120")
+				)
+		);
+	}
 }
