5629fc3852ef0397dc19998ba909a014c738d000
Pedro Furtado <pedro.nazare@pdcase.com.br>
2023-04-23 00:37:04 -0300

    feat: first commit Payroll api

---
+++ payroll-api/src/main/java/com/ppfurtado/payrollapi/PayrollApiApplication.java
@@ -0,0 +1,13 @@
+package com.ppfurtado.payrollapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayrollApiApplication.class, args);
+    }
+
+}
