7224f5d9ee195fcbe48d5a3f3acf346b7546c617
Pedro Furtado <pedro.nazare@pdcase.com.br>
2023-05-05 00:09:56 -0300

    feat: log

--- user-api/src/main/java/com/ppfurtado/userapi/services/impl/UserServiceImpl.java
+++ user-api/src/main/java/com/ppfurtado/userapi/services/impl/UserServiceImpl.java
@@ -5,18 +5,23 @@ import com.ppfurtado.userapi.repositories.UserRepository;
 import com.ppfurtado.userapi.services.UserService;
 import com.ppfurtado.userapi.services.excpetions.ObjectNotFoundExcpetion;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Log4j2
 @RequiredArgsConstructor
 @Service
 public class UserServiceImpl implements UserService {
 
+    private final Environment env;
     private final UserRepository userRepository;
 
     @Override
     public User findById(Long id) {
+        log.info("USER_SERVICE ::: Get Request on " + env.getProperty("local.server.port")+" port");
         return userRepository.findById(id).orElseThrow(() -> new ObjectNotFoundExcpetion("User not found"));
     }
 
